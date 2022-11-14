09384e29d1b918d9bf587dc756bd531b649bdd67
Niioon <schuermeyern@gmail.com>
2022-11-14 20:42:59 +0000

    added navigation control node

---
+++ src/my_robot_control/src/navigation_control.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python 
+
+import rospy
+import roslaunch
+from std_msgs.msg import String
+
+finished_instructions = False
+
+
+def callback(data):
+    # topic gets called when instructions are fulfilled
+    global finished_instructions
+    finished_instructions = True
+
+
+def main():
+
+
+    rospy.init_node('navigation_control')
+
+    # subscribe to qr code topic
+    rospy.Subscriber("qr_done", String, callback)
+    
+    rate = rospy.Rate(1)
+
+    # hard code qr code locations here
+    qr_codes = []
+
+    for qr_code in qr_codes:
+
+        # navigate to qr code
+
+        
+        # call node that exectutes instructions
+        finished = False
+        while not finished:
+            rate.sleep()
+
+    # done, shutdown nodes
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
